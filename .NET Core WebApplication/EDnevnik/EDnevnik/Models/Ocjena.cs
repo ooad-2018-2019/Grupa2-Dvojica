34494f45c676b91958dc4a1443b21edb557e8da5
amarferaget <amarferaget@hotmail.com>
2019-06-11 16:00:32 +0200

    dodavane kalse u Model

---
+++ .NET Core WebApplication/EDnevnik/EDnevnik/Models/Ocjena.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDnevnik.Models
+{
+    public class Ocjena
+    {
+        public int ocjena { get; set; }
+        public DateTime datumUnosa { get; set; }
+        public int idUcenika { get; set; }
+
+    }
+}
